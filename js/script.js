64807fb169907ad5fcb7e55479af19284c9fe83f
m-bender1 <64276163+m-bender1@users.noreply.github.com>
2021-06-02 16:28:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,6 +28,6 @@ function generateProducts(a) {
       prodDiv.appendChild(prodImg);
       prodDiv.appendChild(prodName);
       prodDiv.appendChild(prodPrice);
-      wrapper.appendChild(prodDiv);
+      wrapper[0].append(prodDiv);
    }
 }
