67f92e1cb6d0ecb88c05d10141419044b8659cdb
m-bender1 <64276163+m-bender1@users.noreply.github.com>
2021-06-03 22:58:35 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,8 +13,7 @@ xmlhttp.send();
 function generateProducts(a) {
    // get the wrapper div
    let wrapper = document.getElementsByClassName("grid-wrapper");
-   console.log(a, a.length)
-   for (let i = 0; i < a.length; i++) {
+   for (let i = 0; i < a.products.length; i++) {
       // create the elements to append
       let prodDiv = document.createElement("div");
       let prodImg = document.createElement("img");
