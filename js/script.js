183c3d1e4b395e62292a6d4d2f469ab7034ac610
m-bender1 <mattbender252@yahoo.com>
2021-06-07 15:59:08 -0400

    Product Filtering

--- js/script.js
+++ js/script.js
@@ -11,27 +11,33 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 function generateProducts(a) {
+   // filtering, checks product type <select>
+   // filters based on the selected option
+   // first, get selects value by the id
+   let prodSelect = document.getElementById("productSelect").value;
    // get the wrapper div
    let wrapper = document.getElementsByClassName("grid-wrapper");
    for (let i = 0; i < a.products.length; i++) {
-      // create the elements to append
-      let prodDiv = document.createElement("div");
-      let prodImg = document.createElement("img");
-      let prodPrice = document.createElement("p");
-      let prodName = document.createElement("p");
-      
-      // add classes, src to image and proddiv
-      // add innerhtml to p tags
-      // append the img and p tags to proddiv and then proddiv to grid wrapper
-      prodDiv.classList.add("productDiv");
-      prodImg.src = a.products[i].imageUrl;
-      prodImg.classList.add("productImg");
-      prodPrice.innerHTML = "$" + a.products[i].productPrice
-      prodName.innerHTML = "<b>" + a.products[i].productName + "</b>";
-      prodDiv.appendChild(prodImg);
-      prodDiv.appendChild(prodName);
-      prodDiv.appendChild(prodPrice);
-      wrapper[0].append(prodDiv);
+      if (prodSelect == "All" || prodSelect == a.products[i].productType) {
+         // create the elements to append
+         let prodDiv = document.createElement("div");
+         let prodImg = document.createElement("img");
+         let prodPrice = document.createElement("p");
+         let prodName = document.createElement("p");
+
+         // add classes, src to image and proddiv
+         // add innerhtml to p tags
+         // append the img and p tags to proddiv and then proddiv to grid wrapper
+         prodDiv.classList.add("productDiv");
+         prodImg.src = a.products[i].imageUrl;
+         prodImg.classList.add("productImg");
+         prodPrice.innerHTML = "$" + a.products[i].productPrice
+         prodName.innerHTML = "<b>" + a.products[i].productName + "</b>";
+         prodDiv.appendChild(prodImg);
+         prodDiv.appendChild(prodName);
+         prodDiv.appendChild(prodPrice);
+         wrapper[0].append(prodDiv);
+      }
    }
 }
 
