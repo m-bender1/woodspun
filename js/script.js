a7bfaa256abdc7cb1b5edfcc15ad2f7e25ee1f84
m-bender1 <mattbender252@yahoo.com>
2021-06-08 00:37:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,17 +48,19 @@ function filterProducts() {
    var prodDivs = document.getElementsByClassName("productDiv");
    // add a display=none class to products not matching filter
    for (let i = 0; i < prodDivs.length; i++) {
+      // removes non-selected products
       if (prodDivs[i].getAttribute("name") !== selectedProd && selectedProd !== "All") {
          prodDivs[i].classList.add("disabled");
-         prodDivs[i].classList.remove("productDiv")
-         // now remove disabled from the rest
-         if (prodDivs[i].getAttribute("name") == selectedProd && selectedProd !== "All") {
-            prodDivs[i].classList.remove("disabled");
-         }
+         prodDivs[i].classList.remove("productDiv");
+      }
+      if (prodDivs[i].getAttribute("name") == selectedProd && selectedProd !== "All") {
+         prodDivs[i].classList.remove("disabled");
+         prodDivs[i].classList.add("productDiv");
       }
       if (selectedProd == "All") {
          if (prodDivs[i].classList.contains("disabled")) {
             prodDivs[i].classList.remove("disabled");
+            // ensure it still has productDiv class
             prodDivs[i].classList.add("productDiv");
          }
       }
