a491a686c8ac5fa6aa81d5f68dd670e2f274e219
m-bender1 <mattbender252@yahoo.com>
2021-06-27 16:46:53 -0400

    added logo click to home function

--- js/script.js
+++ js/script.js
@@ -249,3 +249,9 @@ function generateTotal(priceArr, subE, shipE, totalE) {
    shipE.innerHTML = "Shipping: $" + shipping.toFixed(2);
    totalE.innerHTML = "Total: $" + total.toFixed(2);
 }
+
+
+// nav logo onclick home function
+document.getElementsByClassName("logo")[0].addEventListener("click", function () {
+   window.location = "index.html";
+})
